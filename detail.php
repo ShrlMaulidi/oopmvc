cb44bcffc66d44b73df66eed8e11b0b471aaaf32
ShrlMaulidi <sahrulmaulidi294@gmail.com>
2024-05-18 11:11:44 +0700

    Initial commit
    First commit

--- detail.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once "model/anggota_model.php";
-
-$anggota = getAnggotaById($_GET['id']);
-require "view/anggota/detail.php";
--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
-require_once("model/anggota_model.php");
+//Memanggil controller dan model
 
-$anggota = getAnggota();
+require_once "model/anggota_model.php";
+require_once "controller/anggota.php";
 
-require "view/anggota/list.php";
+//routing
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ("/oppmvc/index.php" == $uri){
+    index();
+} else if ("/oopmvc/index.php/detail" == $uri && isset($_GET['id'])){
+    detail($_GET['id']);
+} else {
+    header("HTTP/1.1 404 not found");
+    echo "<html> <body><h1>page not found</h1> </body> </html>";
+}
 ?>
 
